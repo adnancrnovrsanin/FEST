5a64d1ae9dc748129a23509709c5fc695013c45e
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-06-10 16:12:50 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-const App = () => {
-    return (
-        <div>
-            <h1>React Typescript Webpack Starter Template</h1>
+import './App.css';
+import rocketImg from './assets/rocket.png';
+import { Login } from './components/Login';
+import { Signup } from './components/Signup';
+
+function App() {
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <Login  />
         </div>
-    );
+        <div className="col-md-7 my-auto">
+          <img className="img-fluid w-100" src={rocketImg} alt=""/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
