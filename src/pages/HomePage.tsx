a1237ec1031b2e1786e1f1418f7a26a9de58f044
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-06-26 16:39:18 +0200

    Profile Page addition

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import CurrentShows from "../components/CurrentShows/CurrentShows";
 import FeaturedShows from "../components/FeatureShows/FeatureShows";
 import HeroSection from "../components/Hero/Hero";
+import ProfilePage from "./ProfilePage/ProfilePage";
 
 const HomePage = () => {
     return (
