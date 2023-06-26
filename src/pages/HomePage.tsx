4b4b89a83834c9bb0c46bd6fd57575f3fce806d3
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-06-26 15:24:01 +0200

    Minor changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import CurrentShows from "../components/CurrentShows/CurrentShows";
+import FeaturedShows from "../components/FeatureShows/FeatureShows";
+import Footer from "../components/Footer/Footer";
+
 const HomePage = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+        <FeaturedShows/>
+        <CurrentShows/>
         </div>
     );
 }
