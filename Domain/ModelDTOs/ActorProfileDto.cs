73b9ff37209a0aa0231935b4fe1cda8834e48b36
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-06-30 01:43:07 +0200

    DTOs for Profiles

---
+++ Domain/ModelDTOs/ActorProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ModelDTOs
+{
+    public class ActorProfileDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+        public ICollection<ActorShowRoleAudition> Auditions { get; set; }
+        public ICollection<ActorShowRole> ActingRoles { get; set; }
+        public Photo ProfilePicture { get; set; }
+        public ICollection<Photo> Slike { get; set; }
+    }
+}
---
+++ Domain/ModelDTOs/ReviewerProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ModelDTOs
+{
+    public class ReviewerProfileDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+        public ICollection<ShowFestivalApplicationReview> ShowApplications { get; set; }
+        public ICollection<AuditionReview> AuditionReviews { get; set; }
+        public Photo ProfilePicture { get; set; }
+
+    }
+}
