073925c0baf09f89c073893eb85406b8a8780851
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-07-02 13:59:58 +0200

    Minor change

--- Application/Profiles/ActorDetails.cs
+++ Application/Profiles/ActorDetails.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using Domain.ModelDTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Persistance;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +15,29 @@ using System.Threading.Tasks;
 
 namespace Application.Profiles
 {
-    public class Details
+    public class ActorDetails
     {
+        public class Query : IRequest<Result<ActorProfileDto>>
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Query, Result<ActorProfileDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<ActorProfileDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var profile = await _context.Users.Include(p => p.Photos).Include(p => p.Auditions).Include(p => p.ActingRoles).ProjectTo<ActorProfileDto>(_mapper.ConfigurationProvider).ToListAsync();
+                if (profile == null) return null;
+                return  Result<ActorProfileDto>.Success(_mapper.Map<ActorProfileDto>(profile));     
+            }
+        }
     }
 }
--- Application/Profiles/ReviewerDetails.cs
+++ Application/Profiles/ReviewerDetails.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using Domain.ModelDTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Persistance;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +15,29 @@ using System.Threading.Tasks;
 
 namespace Application.Profiles
 {
-    internal class ReviewerDetails
+    public class ReviewerDetails
     {
+        public class Query : IRequest<Result<ReviewerProfileDto>>
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Query, Result<ReviewerProfileDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<ReviewerProfileDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var profile = await _context.Users.Include(p => p.Photos).Include(p=> p.AuditionReviews).ProjectTo<ReviewerProfileDto>(_mapper.ConfigurationProvider).ToListAsync();
+                if (profile == null) return null;
+                return Result<ReviewerProfileDto>.Success(_mapper.Map<ReviewerProfileDto>(profile));
+            }
+        }
     }
 }
