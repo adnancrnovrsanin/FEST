27ab99a568d3bd8f7dd774a4af418a9b2ef29ab0
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-07-04 18:19:40 +0200

    Updated Mapping

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -32,30 +32,16 @@ namespace Application.Core
                 .ForMember(d => d.AverageReview, o => o.MapFrom(s => s.Audition.Reviews.Count == 0 ? 0 : s.Audition.Reviews.Average(r => r.Review)));
             CreateMap<Audition, Audition>();
             CreateMap<Schedule, Schedule>();
-            CreateMap<ScheduleDto, ScheduleDto>();
-            CreateMap<Schedule, ScheduleDto>()
-                .ForMember(sd => sd.FestivalName, o => o.MapFrom(s => s.Festival.Name))
-                .ForMember(sd => sd.TheatreName, o => o.MapFrom(s => s.TheatreShow.Theatre.Name))
-                .ForMember(sd => sd.ShowName, o => o.MapFrom(s => s.TheatreShow.Show.Name))
-                .ForMember(sd => sd.FestivalId, o => o.MapFrom(s => s.Festival.Id))
-                .ForMember(sd => sd.TheatreId, o => o.MapFrom(s => s.TheatreShow.Theatre.Id))
-                .ForMember(sd => sd.ShowId, o => o.MapFrom(s => s.TheatreShow.Show.Id))
-                .ForMember(sd => sd.TimeOfPlay, o => o.MapFrom(s => s.TimeOfPlay.GetValueOrDefault().ToUniversalTime()));
-            CreateMap<ScheduleDto, Schedule>()
-                .ForMember(s => s.TimeOfPlay, o => o.MapFrom(sd => DateTime.Parse(sd.TimeOfPlay, null, System.Globalization.DateTimeStyles.RoundtripKind)));
             CreateMap<Show, Show>();
             CreateMap<Theatre, Theatre>();
             CreateMap<Festival, FestivalDto>()
                 .ForMember(d => d.StartDate, o => o.MapFrom(s => s.StartDate.ToUniversalTime()))
                 .ForMember(d => d.EndDate, o => o.MapFrom(s => s.EndDate.ToUniversalTime()));
-            CreateMap<Theatre, TheatreDto>()
-                .ForMember(d => d.ManagerEmail, o => o.MapFrom(s => s.Manager.Email));
+            CreateMap<Theatre, TheatreDto>();
             CreateMap<TheatreDto, Theatre>();
             CreateMap<FestivalDto, Festival>()
                 .ForMember(d => d.StartDate, o => o.MapFrom(s => DateTime.Parse(s.StartDate, null, System.Globalization.DateTimeStyles.RoundtripKind)))
                 .ForMember(d => d.EndDate, o => o.MapFrom(s => DateTime.Parse(s.EndDate, null, System.Globalization.DateTimeStyles.RoundtripKind)));
-            CreateMap<ActorProfileDto, ActorProfileDto>();
-            CreateMap<ActorProfileDto, AppUser>();
             CreateMap<AppUser, ActorProfileDto>()
                 .ForMember(d => d.ProfilePicture, o => o.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain)))
                 .ForMember(d => d.Photos, o => o.MapFrom(s => s.Photos.Where(p => !p.IsMain)))
@@ -63,19 +49,8 @@ namespace Application.Core
                 .ForMember(d => d.AuditionsNotReviewed, o => o.MapFrom(s => s.Auditions.Where(a => a.Audition.Reviews.Count < 3)));
             CreateMap<AppUser, ReviewerProfileDto>()
                 .ForMember(d => d.ProfilePicture, o => o.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain)));
-            CreateMap<ShowRole, ShowRoleDto>();
-            CreateMap<ShowRoleDto, ShowRole>();
-            CreateMap<ShowRole, ShowRole>();
-            CreateMap<ShowFestivalApplication, ShowFestivalApplication>();
-            CreateMap<ShowFestivalApplicationDto, ShowFestivalApplicationDto>();
-            CreateMap<ShowFestivalApplicationDto, ShowFestivalApplication>();
-            CreateMap<ShowFestivalApplication, ShowFestivalApplicationDto>()
-                .ForMember(d => d.SerialNumber, o => o.MapFrom(s => s.Show.SerialNumber))
-                .ForMember(d => d.Name, o => o.MapFrom(s => s.Show.Name))
-                .ForMember(d => d.DirectorName, o => o.MapFrom(s => s.Show.DirectorName))
-                .ForMember(d => d.StoryWriterName, o => o.MapFrom(s => s.Show.StoryWriterName))
-                .ForMember(d => d.LengthOfPlay, o => o.MapFrom(s => s.Show.LengthOfPlay))
-                .ForMember(d => d.AdditionalInformation, o => o.MapFrom(s => s.Show.AdditionalInformation));
+            CreateMap<ActorShowRole, ActorShowRoleDto>();   
+
         }
     }
 }
